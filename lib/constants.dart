0191ae10807ab9ba9cf7a35bf46585747c1a8aec
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-27 12:51:51 +0100

    separating consts

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 const IconData bag = Icons.shopping_bag;
 const IconData cart = Icons.shopping_cart;
 const IconData card = Icons.credit_card;
-const Icon check = Icon(Icons.check, color: Colors.green,size: 46.0,);
+const Icon check = Icon(Icons.check, color: right,size: greenIcon,);
 // textStyles
 const TextStyle button = TextStyle(
     color: Color.fromARGB(255, 192, 174, 21)
@@ -14,5 +14,10 @@ const TextStyle animated = TextStyle(fontSize: 20);
 const Color containerClr = Colors.black;
 const Color iconColors = Colors.white;
 const Color yellow = Color.fromARGB(255, 192, 174, 21);
+const Color right = Colors.green;
  // titles
 const String btnTitle = "Buy Now";
+
+// sizes
+const double greenIcon = 46;
+const double defaultIconSize = 30;
